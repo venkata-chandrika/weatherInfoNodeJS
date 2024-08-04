bad439e404f53c9bfc0ba8da6ea0571eff93fde3
venkata-chandrika <balimidivenkatachandrika@gmail.com>
2024-08-04 14:30:53 +0530

    first commit

---
+++ utils/weatherData.js
@@ -0,0 +1,23 @@
+const request = require("request");
+
+const openWeatherMap = {
+  BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+  SECRET_KEY: "0c8939697b588a0d9ba7786f9320177d",
+};
+
+const weatherData = (address, callback) => {
+  const url =
+    openWeatherMap.BASE_URL +
+    encodeURIComponent(address) +
+    "&APPID=" +
+    openWeatherMap.SECRET_KEY;
+  console.log(url);
+  request({ url, json: true }, (error, data) => {
+    if (error) {
+      callback(true, "Unable to fetch data, Please try again. " + error);
+    }
+    callback(false, data?.body);
+  });
+};
+
+module.exports = weatherData;
